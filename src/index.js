89a9502630c523cd830018ddbfd2b610a8a82e7e
Owen Crow <owen.crow@gmail.com>
2020-09-08 22:35:41 -0500

    After exercise 1.1

--- src/index.js
+++ src/index.js
@@ -1,25 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Hello = (props) => {
+const Header = (props) => {
+	// <Header course={course} />
 	  return (
 		      <div>
-		        <p>Hello, {props.name}</p>
-		{console.log("test comment")}
+		        <h1>
+		          {props.course}
+		        </h1>
+		      </div>
+		    )
+}
+const Content = (props) => {
+	//<Content part={part1} exercise={exercises1} />
+	return (
+		      <div>
+		        <p>
+		           {props.part} {props.exercise}
+		        </p>
+		      </div>
+		    )
+}
+
+const Total = (props) => {
+	  return (
+		      <div>
+		        <p>
+		          Number of exercises {props.total}
+		        </p>
 		      </div>
 		    )
 }
 
 const App = () => {
+	  const course = 'Half Stack application development'
+	  const part1 = 'Fundamentals of React'
+	  const exercises1 = 10
+	  const part2 = 'Using props to pass data'
+	  const exercises2 = 7
+	  const part3 = 'State of a component'
+	  const exercises3 = 14
+
 	  return (
 		      <div>
-		        <h1>Greetings</h1>
-		        <Hello name="George"/>
-		        <Hello name="Daisy"/>
+		        <Header course={course} />
+		  	<Content part={part1} exercise={exercises1} />
+		  	<Content part={part2} exercise={exercises2} />
+		  	<Content part={part3} exercise={exercises3} />
+		        <Total total={exercises1 + exercises2 + exercises3} />
 		      </div>
 		    )
 }
-
-
 
 ReactDOM.render(<App />, document.getElementById('root'))
