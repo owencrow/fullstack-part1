839143ec152ceb3073fba46561b36c353bdf4449
Owen Crow <owen.crow@gmail.com>
2020-09-13 23:05:09 -0500

    Part 1c-2

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
+const Display = ({counter}) => <div>{counter}</div>
+
+const Button = ({handleClick,text}) => {
+	return (
+		<button onClick={handleClick}>
+		{text}
+		</button>
+	)
+}
+
+
 const App = () => {
 	const [ counter, setCounter ] = useState(0)
 
-	setTimeout(
-		() => setCounter(counter + 1),
-		1000
-	)
+	const increaseByOne = () => setCounter(counter + 1)
+	const decreaseByOne = () => setCounter(counter - 1)
+
+	const setToZero = () => setCounter(0)
 
 	return (
-		<div>{counter}</div>
+		<div>
+		<Display counter={counter}/>
+		<Button
+		handleClick={increaseByOne}
+		text='plus'
+		/>
+		<Button
+		handleClick={setToZero}
+		text='zero'
+		/>     
+		<Button
+		handleClick={decreaseByOne}
+		text='minus'
+		/>           
+		</div>
 	)
+
 }
 
 let counter = 1
@@ -23,10 +49,10 @@ const refresh = () => {
 		document.getElementById('root')
 	)
 }
-refresh()
-counter += 1
-refresh()
-counter += 1
+//refresh()
+//counter += 1
+//refresh()
+//counter += 1
 refresh()
 
 // ReactDOM.render(<App />, document.getElementById('root'))
